0d20495236d5130289bfd3b6f38994b2e43cd427
badosz0 <m@badosz.com>
2022-06-09 23:39:20 +0200

    fix indentation

--- src/structures/token.ts
+++ src/structures/token.ts
@@ -2,10 +2,10 @@ import { Source } from './source';
 
 export enum TokenType {
   UNKNOWN = 'unknown', // TODO: throw error if lexer returns at least 1 unknown token
-	NUMBER = 'number',
-	STRING = 'string',
-	KEYWORD = 'keyword',
-	SYMBOL = 'symbol',
+  NUMBER = 'number',
+  STRING = 'string',
+  KEYWORD = 'keyword',
+  SYMBOL = 'symbol',
 }
 
 export class Token {
