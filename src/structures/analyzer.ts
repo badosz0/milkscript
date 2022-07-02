5abbaba3d7446e82e13c268e5fd6df16d4f5bba2
badosz0 <m@badosz.com>
2022-07-02 19:34:58 +0200

    analyzer

---
+++ src/structures/analyzer.ts
@@ -0,0 +1,21 @@
+import { assert } from 'node:console';
+import { Node } from './node';
+import { Program } from './nodes/program';
+
+export class Analyzer {
+  private program: Program;
+
+  constructor(program: Program) {
+    this.program = program;
+  }
+
+  private walk(node: Node): void {
+    assert(false, node.name);
+  }
+
+  public analyze(): void {
+    this.program.nodes.forEach((node) => {
+      this.walk(node);
+    });
+  }
+}
